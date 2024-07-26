ebdb6b71b5d417095d822d04df879bced0e800ac
zaidasfi002 <zaid.asfi23@outlook.com>
2024-07-26 15:13:41 +0530

    task3 of js

---
+++ JavaScript/task2.js
@@ -0,0 +1,10 @@
+let name = "John";
+let age = 30;
+let isStudent = true;
+let height = null;
+let weight;
+let person = { 
+          firstName: "Bill", 
+          lastName: "Gates" };
+let colors = ["red", "green", "blue"];
+console.log(name, age, isStudent, height, weight, person, colors);
---
+++ JavaScript/task3.js
@@ -0,0 +1,46 @@
+let a=10;
+let b=20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+if(a>b)
+{
+          console.log("a is grater than b");
+}else
+{
+          console.log("b is greater than a");
+}
+
+
+switch (a) {
+          case 10:
+                    console.log("a is 10");
+                    break;
+
+          default:
+                    console.log("a is not 10");
+                    break;
+}
+
+for(let i=0;i<5;i++)
+{
+          console.log(i)
+}
+
+let j=0;
+while(j<5)
+{
+console.log(j)
+j++;
+}
+
+let k=0;
+do{
+          console.log(k)
+          k++;
+}while(k<5)
